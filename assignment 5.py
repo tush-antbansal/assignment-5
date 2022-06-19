6f99ca7f25a9b5dd66798d1665e18bff12550d9f
tush-antbansal <101082652+tush-antbansal@users.noreply.github.com>
2022-06-19 19:25:00 +0530

    Update assignment 5.py

--- assignment 5.py
+++ assignment 5.py
@@ -1,29 +1,30 @@
 # NAME: Tushant Bansal
 # SID: 21102084
 # BRANCH : CIVIL
-
+import math
 #question-1
-string=input("Enter the the string :")
-print(string[::-1])
- 
+str = input("Please enter the string: ")
+nstr =""
+for i in range(len(str)-1,-1,-1):  
+    nstr = nstr + str[i]
+print(nstr)
 
 #question-2
 x=int(input("Enter the minimum value :"))
 y=int(input("Enter the maximum value: "))
 num=int(input("Enter the number for divisibility check :"))
-i=x
-while i<=y:
-    if i%num==0 :
-        print(i)
-    i+=1
+while x<=y:
+    if x%num==0 :
+        print(x)
+    x+=1
 
 #question-3
 a=float(input("Enter the length of side:"))
 b=float(input("Enter the length of side:"))
 c=float(input("Enter the length of side:"))
-if a+b>c and a+c>b and b+c>a :
+if a+b>c or a+c>b or b+c>a :
     s=(a+b+c)/2
-    ar_tri=(s*(s-a)*(s-b)*(s-c))**(0.5)
+    ar_tri= math.sqrt(s*(s-a)*(s-b)*(s-c))
     print(ar_tri)
 else :
     print("Error")
@@ -31,12 +32,12 @@ else :
 #question-4
 n=int(input("Enter the no. of rows :"))
 t=(n//2)+1
-l=n//2
+c=n//2
 for i in range(1,t+1):
     for j in range(1,i+1):
         print("*",end="")
     print()
-for q in range(l,0,-1):
+for q in range(c,0,-1):
     for r in range(q,0,-1):
         print("*",end="")
     print()
@@ -90,9 +91,8 @@ odd=[]
 even=[]
 times={}
 li=[]
-num=int(input("Enter the number "))
 for i in range(10):
-    
+    num=int(input("Enter the number "))
     li.append(num)
     if num>=0:
         pos.append(num)
@@ -115,12 +115,11 @@ print("Number of times each number occurs in the List",times)
 
 
 
-
 #question-9
 n=int(input("Number of words: "))
 li=[]
-word=input("Enter the word: ")
 for i in range(n):
+    word=input("Enter the word: ")
     li.append(word)
 times={}
 for i in li :
@@ -129,5 +128,6 @@ for i in li :
     else :
         times[i]+=1
 print("Number of occurences: ",times)
+
 
 
